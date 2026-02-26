dbcaa9aa895dd9f2cb4aa4fa604756bd41907c19
RMcVelia <rick.mcvelia@gmail.com>
2026-02-26 19:23:08 +0000

    Update azurerm and terraform versions
    - azurerm 4.61.0
    - terraform 1.14.5

--- terraform/domains/infrastructure/main.tf
+++ terraform/domains/infrastructure/main.tf
@@ -1,5 +1,5 @@
 module "domains_infrastructure" {
-  source                 = "./vendor/modules/domains//domains/infrastructure"
+  source                 = "./vendor/modules/aks//domains/infrastructure"
   hosted_zone            = var.hosted_zone
   deploy_default_records = var.deploy_default_records
 }
